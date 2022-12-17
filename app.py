33946bce1317e27ff44b560b44813f8e27446898
hugotomita1201 <70402339+hugotomita1201@users.noreply.github.com>
2022-12-17 12:29:53 -0800

    lets go

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-
-
-import streamlit as st
+import streamlit as stv
 
 st.header('Tossing a Coin')
+
+st.header('Tossing a coin')
+
+number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
+start_button = st.button('Run')
+
+if start_button: 
+    st.write(f'Running the experiment of {number_of_trials} trials.')
 
 st.write('It is not a functional application yet. Under construction.') 
